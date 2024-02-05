c0695d6b71912dc65632128e9ee29d109e5a7ff8
Aparajita Kumari <156213356+kumaparajita104@users.noreply.github.com>
2024-02-05 10:24:57 +0530

    Add files via upload

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,51 @@
-print("Hello world Hello world hell")
+#! /usr/bin/python3
+# This Phython program will print Hellow World...
+print("Hello World Demo...Jan 2024...\n")
+print("Hello World Demo...Jan 2024...\n")
+print("Hello World Demo...Jan 2024...\n")
+print("Hello World Demo...Jan 2024...\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
